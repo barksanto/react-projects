f716458f9158195c745979d01cc57ecd08a95e39
Barkley Santo <barkley.santo@gmail.com>
2021-02-14 12:28:24 -0500

    if no yours left, render a message saying there are no more

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -37,6 +37,13 @@ function App() {
       <Loading />
     </main>
   }
+  if (tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>No Tours Left</h2>
+      </div>
+    </main>
+  }
   return <main><Tours tours={tours} removeTour={removeTour} /></main>
 }
 
