47129b79955be605f3e68ee5168cf5ba1075d205
Barkley Santo <barkley.santo@gmail.com>
2021-02-17 12:39:11 -0500

    use imported icon components for + and -

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Question = ({ title, info }) => {
-  const [readMore, setReadMore] = useState(false);
-
+  const [showInfo, setShowInfo] = useState(false);
 
   return (<article className="question">
     <header>
       <h4>{title}</h4>
-      <button className="btn" onClick={() => setReadMore(!readMore)}>{readMore ? '👎🏾' : '👍🏾'}</button>
+      <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+        {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+      </button>
     </header>
-    {/* <p>{readMore ? info : `${info.substring(0, 75)}...`}</p> */}
-    <p>{readMore ? info : ''}</p>
+    {/* <p>{showInfo ? info : `${info.substring(0, 75)}...`}</p> */}
+    <p>{showInfo ? info : ''}</p>
   </article >)
 };
 
 export default Question;
+
+
