d2cbf7253a78d744b2944271cb874b21c67982ed
Barkley Santo <barkley.santo@gmail.com>
2021-02-17 14:21:13 -0500

    set loading default value to true (this is a piece of state) - if loading is true, show laoding tag

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -3,8 +3,31 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setloading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setJobs(newJobs);
+    setloading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>)
+  }
+
+  return <h2>jobs</h2>
 }
 
 export default App
