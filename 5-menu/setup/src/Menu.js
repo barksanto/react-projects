6efe0f8cb1c62bca2666012bfe9a08ead6776f29
Barkley Santo <barkley.santo@gmail.com>
2021-02-17 13:24:15 -0500

    show description just under the hader with price and title

--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Menu = ({ items }) => {
   return <div className="section-center">
     {items.map((menuItem) => {
+      console.log(menuItem)
       const { id, title, img, desc, price } = menuItem;
       return <article key={id} className="menu-item">
         <img className="photo" src={img} alt={title} />
         <div className="item-info">
           <header>
             <h4>{title}</h4>
-            <h4>{price}</h4>
+            <h4 className="price">{price}</h4>
           </header>
+          <p className="item-text">{desc}</p>
         </div>
       </article>
     })}
