017817f16c059e271a4c967739d1515f3aa5c696
Barkley Santo <barkley.santo@gmail.com>
2021-02-17 13:34:12 -0500

    fiter button works for one category and breakfast

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -8,6 +8,15 @@ function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
+  // always filter from original list instead of the modified list
+  const filterItems = (category) => {
+    if(category === "all"){
+      setMenuItems(items);
+      return
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
 
   return (
     <main>
@@ -15,7 +24,7 @@ function App() {
         <div className="title">
           <h2>our menu</h2>
           <div className="underline"></div>
-          <Categories />
+          <Categories filterItems={filterItems} />
           {/* pass in menuItems state value as a prop */}
           <Menu items={menuItems} />
         </div>
