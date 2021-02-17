3ce78ea8f720a26ef71d65a3bfc988cac8663b68
Barkley Santo <barkley.santo@gmail.com>
2021-02-17 13:22:36 -0500

    destructure price and display to screen in component

--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -3,9 +3,15 @@ import React from 'react';
 const Menu = ({ items }) => {
   return <div className="section-center">
     {items.map((menuItem) => {
-      const { id, title, img, desc } = menuItem;
+      const { id, title, img, desc, price } = menuItem;
       return <article key={id} className="menu-item">
         <img className="photo" src={img} alt={title} />
+        <div className="item-info">
+          <header>
+            <h4>{title}</h4>
+            <h4>{price}</h4>
+          </header>
+        </div>
       </article>
     })}
   </div>;
